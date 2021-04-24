ea8085eba26f3890e1cc0d92b737a2d1e8cd132b
PatWil <patrick.williams@tlgcohort.com>
2021-04-24 12:10:28 -0700

    bigger mock db

--- Db/index.js
+++ Db/index.js
@@ -15,6 +15,22 @@ const db = [
       "https://images.unsplash.com/photo-1494449880320-18d3dae5d16e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3RhdHVlJTIwb2YlMjBsaWJlcnR5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
     description: "land mark",
   },
+  {
+    uid: "000003",
+    name: "navy pier",
+    location: "chicago",
+    photo:
+      "https://images.unsplash.com/photo-1437252828125-16e5946d9766?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bmF2eSUyMHBpZXJ8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+    description: "land mark",
+  },
+  {
+    uid: "000004",
+    name: "sparty",
+    location: "michigan",
+    photo:
+      "https://images.unsplash.com/photo-1562361241-8681b566232d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWljaGlnYW4lMjBzdGF0ZSUyMHVuaXZlcnNpdHl8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+    description: "land mark",
+  },
 ];
 module.exports = {
   db,
